5c09d2f2b2a3edf4e11e384a56821bc3af20919b
shaohui-jin <1061131737@qq.com>
2025-11-02 18:53:13 +0800

    打包配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -88,7 +88,7 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
         // languages: ['javascript', 'typescript'],
         languageWorkers: [ 'editorWorkerService', 'json', 'typescript', 'html', 'css'],
         // publicPath: 'https://unpkg.com/vite-plugin-monaco-editor@1.0.5/cdn',
-        publicPath: 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs/loader.min.js'
+        // publicPath: 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs/loader.min.js'
       })
       // 打包分析
     ],
