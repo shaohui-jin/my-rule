fe2e4004c70970ab16d0fd880df04eb1a99ae964
shaohui-jin <1061131737@qq.com>
2025-11-02 17:36:12 +0800

    打包配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,96 @@
-import dayjs from 'dayjs'
-import { resolve } from 'path'
 import pkg from './package.json'
-import { getPluginsList } from './build/plugins'
-import { UserConfigExport, ConfigEnv } from 'vite'
+import { UserConfigExport, ConfigEnv, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import DefineOptions from 'unplugin-vue-define-options/vite'
+import removeConsole from 'vite-plugin-remove-console'
+import dayjs, { Dayjs } from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+import { green, bold } from 'picocolors'
+import { getPackageSize } from '@pureadmin/utils'
+import svgLoader from 'vite-svg-loader'
+dayjs.extend(duration)
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+import path from 'path'
 
 /** 当前执行node命令时文件夹的地址（工作目录） */
 const root: string = process.cwd()
 
-/** 路径查找 */
-const pathResolve = (dir: string): string => {
-  return resolve(__dirname, '.', dir)
-}
-
 const { dependencies, devDependencies, name, version } = pkg
-const __APP_INFO__ = {
-  pkg: { dependencies, devDependencies, name, version },
-  lastBuildTime: dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss')
-}
 
 export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  const env = loadEnv(mode, process.cwd())
+  let config: { command: string }
+  let startTime: Dayjs
+  let endTime: Dayjs
+  let outDir: string
   return {
-    base: './',
+    base: env.VITE_PUBLIC_PATH,
     root,
     resolve: {
       alias: {
-        '@': pathResolve('src'),
-        '@build': pathResolve('build')
+        '@': path.resolve(__dirname, 'src')
       }
     },
-    // 服务端渲染
     server: {
-      // 是否开启 https
-      // 端口号
       port: 8001,
-      host: '0.0.0.0',
-      proxy: {
-        '/api': {
-          // target: 'http://10.8.90.16:8080',
-          target: 'http://api-dev.neurohome-ai.com',
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
-        }
-      }
+      host: '0.0.0.0'
     },
-    plugins: getPluginsList(command),
+    plugins: [
+      vue(),
+      // jsx、tsx语法支持
+      vueJsx(),
+      DefineOptions(),
+      // 线上环境删除console
+      removeConsole({ external: ['src/assets/iconfont/iconfont.js'] }),
+      {
+        name: 'vite:buildInfo',
+        configResolved(resolvedConfig) {
+          config = resolvedConfig
+          outDir = resolvedConfig.build?.outDir ?? 'dist'
+        },
+        buildStart() {
+          if (config.command === 'build') {
+            startTime = dayjs(new Date())
+          }
+        },
+        closeBundle() {
+          if (config.command === 'build') {
+            endTime = dayjs(new Date())
+            getPackageSize({
+              folder: outDir,
+              callback: (size: string) => {
+                console.log(
+                  bold(
+                    green(
+                      `🎉恭喜打包完成（总用时${dayjs
+                        .duration(endTime.diff(startTime))
+                        .format('mm分ss秒')}，打包后的大小为${size}）`
+                    )
+                  )
+                )
+              }
+            })
+          }
+        }
+      },
+      // 自定义主题
+      // themePreprocessorPlugin({
+      //   scss: {
+      //     multipleScopeVars: genScssMultipleScopeVars(),
+      //     extract: true
+      //   }
+      // }),
+      // svg组件化支持
+      svgLoader(),
+      // ElementPlus({}),
+      monacoEditorPlugin({
+        languages: ['javascript', 'typescript']
+        // languageWorkers: [ 'editorWorkerService', 'json', 'typescript', 'html', 'css'],
+        // publicPath: 'https://unpkg.com/vite-plugin-monaco-editor@1.0.5/cdn'
+      })
+      // 打包分析
+    ],
     // https://cn.vitejs.dev/config/dep-optimization-options.html#dep-optimization-options
     optimizeDeps: {
       include: [
@@ -64,7 +111,7 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       exclude: [
         '@iconify-icons/ep',
         '@iconify-icons/ri',
-        '@pureadmin/theme/dist/browser-utils',
+        '@pureadmin/theme/dist/browser-utils'
         // '@types/node'
       ]
     },
@@ -77,7 +124,7 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
         // 确保在构建时包含外部依赖
         external: ['monaco-editor'],
         input: {
-          index: pathResolve('index.html')
+          index: path.resolve(__dirname, 'index.html'),
         },
         // 静态资源分类打包
         output: {
@@ -86,10 +133,6 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
           assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
         }
       }
-    },
-    define: {
-      __INTLIFY_PROD_DEVTOOLS__: false,
-      __APP_INFO__: JSON.stringify(__APP_INFO__)
     }
   }
 }
